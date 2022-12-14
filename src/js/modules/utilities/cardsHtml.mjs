8ada29179d5021c217616a0ff5565fb93dc7a8e0
Zycrix <Doffa98@live.no>
2022-12-14 19:29:59 +0100

    Path fixing

--- src/js/modules/utilities/cardsHtml.mjs
+++ src/js/modules/utilities/cardsHtml.mjs
@@ -23,7 +23,7 @@ export function createCards(data, counter, index){
     container.innerHTML += `
     <div class = "card m-3 rounded w-100">
       <img src = "${data[i].media[0] || ''}" class = "image-height-fix" alt = "product image" onerror = "this.src = '../media/pexels-luis-del-río-15286.jpg'">
-      <a href = "../pages/specific.html?id=${data[i].id}" class = "unset">
+      <a href = "pages/specific.html?id=${data[i].id}" class = "unset">
         <div class = "card-img-overlay overlay text-light" data-id = "${data[i].id}">
           <h2 class = "card-title">${data[i].title}</h2>
           <p class = "card-text">Posted:</p>
